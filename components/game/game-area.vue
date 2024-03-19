013bd3f69068314784acfb863b510ec92b2547b3
Hanlaoss <asd159632874@gmail.com>
2024-03-19 10:07:21 +0800

    game-list

--- components/game/game-area.vue
+++ components/game/game-area.vue
@@ -26,7 +26,7 @@ onMounted(async () => {
     try {
         const response = await axios.get("https://localhost:7048/api/Games");
         games.value = response.data;
-        console.log(response.data);
+        console.log(games.value);
     } catch (error) {
         console.log(error);
     }
--- components/game/game-list.vue
+++ components/game/game-list.vue
@@ -1,8 +1,34 @@
 <template>
     <div>
         <div v-for="game in gameData" :key="game.id">
-            <h2>{{ game.name }}</h2>
-            <p>{{ game.description }}</p>
+            <nuxt-link :to="`/game-detail/${game.id}`">
+                <div class="row mb-2">
+                    <div class="col-3">
+                        <img
+                            :src="
+                                'images/games/cover/' +
+                                game.developerId +
+                                '/' +
+                                game.id +
+                                '/' +
+                                game.cover
+                            "
+                            width="200"
+                            class=""
+                        />
+                    </div>
+                    <div class="col-7 d-flex align-items-center">
+                        <h3 class="">{{ game.name }}</h3>
+                    </div>
+                    <div class="col-2 d-flex align-items-center">
+                        <p class="text-decoration-line-through text-start">
+                            $ {{ game.price }}
+                        </p>
+                        <span>&nbsp;&nbsp;&nbsp;</span>
+                        <p class="fw-bold">$ {{ game.discountPrice }}</p>
+                    </div>
+                </div>
+            </nuxt-link>
         </div>
     </div>
 </template>
