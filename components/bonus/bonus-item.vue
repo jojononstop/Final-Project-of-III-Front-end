64e0d453d36c895dba073b5527296cb03234fff0
ArukaTarasi <aruka16661@gmail.com>
2024-03-20 18:56:04 +0800

    完成主要商品顯示區塊的每個單一商品可以顯示以及找到對應的名稱與類型 詳見BonusReMe.txt

---
+++ components/bonus/bonus-item.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="shop__item">
+    <div class="shop__item-thumb">
+      <nuxt-link :to="`/shop-details/${bonusProduct.id}`">
+        <img :src="`/images/bonus/${bonusProduct.productTypeId}/${bonusProduct.url}`" />
+      </nuxt-link>
+      <!-- 最愛按鈕 -->
+      <nuxt-link to="#" class="wishlist-button">
+        <i class="far fa-heart"></i>
+      </nuxt-link>
+    </div>
+
+    <div class="shop__item-line"></div>
+    <div class="shop__item-content">
+      <div class="shop__item-content-top">
+        <h4 class="title">
+          <nuxt-link :to="`/shop-details/${bonusProduct.id}`">{{
+            bonusProduct.name
+          }}</nuxt-link>
+        </h4>
+        <div class="shop__item-price">${{ bonusProduct.price }}</div>
+      </div>
+      <div class="shop__item-cat">
+        <nuxt-link to="/shop">{{ getProductTypeName(bonusProduct.productTypeId) }}</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  bonusProduct: Object,
+  bonusProductTypes: Object
+});
+
+function getProductTypeName(productTypeId) 
+{
+  //都有傳進來
+  if (props.bonusProductTypes && props.bonusProductTypes[productTypeId])
+   {
+    //傳出對應ID的名稱
+    return props.bonusProductTypes[productTypeId].name;
+  } 
+  else 
+  {
+    return '';
+  }
+}
+</script>
