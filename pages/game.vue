dcdbe65b60d2bac30ae4321be4ba2c24621c5029
Hanlaoss <asd159632874@gmail.com>
2024-04-07 05:56:28 +0800

    #

--- pages/game.vue
+++ pages/game.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <!-- breadcrumb area start -->
-        <breadcrumb-three title="GAME" subtitle="GAME LIST"></breadcrumb-three>
+        <breadcrumb-three title="遊戲" subtitle="遊戲列表"></breadcrumb-three>
+
         <!-- breadcrumb area end -->
 
         <!-- blog area start -->
@@ -11,15 +12,5 @@
 </template>
 
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
-useSeoMeta({ title: "Game - MYKD" });
-
-const route = useRoute();
-const router = useRouter();
-// if(route.query.page == undefined){
-//      router.push({ query: { ...route.query, page: 1 }});
-// }
-  
-
-
+useSeoMeta({ title: "遊戲 - RIZZ" });
 </script>
