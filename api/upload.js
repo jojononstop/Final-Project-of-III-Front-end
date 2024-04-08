89b78fa708dd152d1d97da37e1c17a89b530be31
xftfchgg <k6663668@gmail.com>
2024-04-08 14:22:53 +0800

    頭像上傳

--- api/upload.js
+++ api/upload.js
@@ -1,44 +1,7 @@
-import formidable from "formidable";
-import fs from "fs";
-import path from "path";
-
-export default defineEventHandler(async (event) => {
-  let imageUrl = "";
-  let oldPath = "";
-  let newPath = "";
-
-  const form = formidable({ multiples: true });
-  const data = await new Promise((resolve, reject) => {
-    form.parse(event.req, (err, fields, files) => {
-      if (err) {
-        reject(err);
-      }
-      if (!files.photo) {
-        resolve({
-          status: "error",
-          message: "Please upload a photo with name photo in the form",
-        });
-      }
-      if (files.photo.mimetype.startsWith("image/")) {
-        let imageName =
-          Date.now() +
-          Math.round(Math.random() * 100000) +
-          files.photo.originalFilename;
-        oldPath = files.photo.filepath;
-        newPath = `${path.join("public", "uploads", imageName)}`;
-        imageUrl = "./public/upload/" + imageName;
-        fs.copyFileSync(oldPath, newPath);
-        resolve({
-          status: "ok",
-          url: imageUrl,
-        });
-      } else {
-        resolve({
-          status: "error",
-          message: "Please upload nothing but images.",
-        });
-      }
-    });
-  });
-  return data;
+export default eventHandler(async (event) => {
+  const { files } = event.context.formidable;
+  console.log(files);
+  return {
+    ok: true,
+  };
 });
